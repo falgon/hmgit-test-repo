a75509610d912ca06a1a5d319406058e8262ab28
roki <falgon53@yahoo.co.jp>
2021-05-13 03:47:40 +0900

    initial commit

---
+++ foo/bar/hoge/piyo/cot.hs
@@ -0,0 +1,5 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-15.5
+
+main :: IO ()
+main = putStrLn "cot"
---
+++ foo/bar/var/hello.hs
@@ -0,0 +1,6 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-15.5
+module Hello where
+
+main :: IO ()
+main = putStrLn "hello"
